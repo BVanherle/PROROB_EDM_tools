131cc987f0bd638f9d12a4f60f86b98b2840eeac
Bram Vanherle <bram.vanherle@uhasselt.be>
2021-09-03 11:14:18 +0200

    Added project with samples

---
+++ PROROB_EDM_tools_samples/Assets/Scripts/PainterControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EDMTools;
+
+public class PainterControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ObjectPainter painter;
+
+    void Start()
+    {
+        painter.ShowCursor(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        painter.SetPosition(transform.position, transform.forward);
+        if (painter.OnTarget())
+        {
+            painter.CreateStroke();
+        } else
+        {
+            Debug.Log("Paint missed target");
+        }
+        
+    }
+}
